aabafee68a81a89ac7f2b2eba9590e424bb47c77
maksimpro001 <108784501+maksimpro001@users.noreply.github.com>
2023-02-08 18:38:41 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function P_S(x, y) {
+  P = (x + y) * 2;
+  S = x * y;
+  alert("Периметер: " + P);
+  alert("Площа: " + S);
+}
+
+P_S(10, 7);
+
+a = prompt("Enter number: ");
+alert((a + 10) * 20 - 30  / 5)
